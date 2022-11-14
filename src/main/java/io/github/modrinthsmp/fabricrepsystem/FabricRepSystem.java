985ed7f087e50e7862eed899fcd24b4efe0ae9be
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-11-14 12:35:37 -0600

    Add loading
    Co-authored-by: stygigoth

--- src/main/java/io/github/modrinthsmp/fabricrepsystem/FabricRepSystem.java
+++ src/main/java/io/github/modrinthsmp/fabricrepsystem/FabricRepSystem.java
@@ -1,21 +1,47 @@
 package io.github.modrinthsmp.fabricrepsystem;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.command.EntitySelector;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.LiteralText;
+import net.minecraft.util.WorldSavePath;
+import org.slf4j.Logger;
+
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.UUID;
 
 import static net.minecraft.command.argument.EntityArgumentType.getPlayer;
 import static net.minecraft.command.argument.EntityArgumentType.player;
 
 public class FabricRepSystem implements ModInitializer {
+    public static final Gson GSON = new GsonBuilder().create();
+    public static final Logger LOGGER = LogUtils.getLogger();
+    public Map<UUID, ReputationData> reputation;
+
     @Override
     public void onInitialize() {
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            try (final Reader reader = Files.newBufferedReader(getReputationPath(server), StandardCharsets.UTF_8)) {
+                reputation = GSON.fromJson(reader, new TypeToken<Map<UUID, ReputationData>>() {}.getType());
+            } catch (Exception e) {
+                LOGGER.error("Failed to load reputation", e);
+            }
+        });
+
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(LiteralArgumentBuilder.<ServerCommandSource>literal("rep")
                 .then(LiteralArgumentBuilder.<ServerCommandSource>literal("view")
@@ -26,6 +52,10 @@ public class FabricRepSystem implements ModInitializer {
                 )
             );
         });
+    }
+
+    public static Path getReputationPath(MinecraftServer server) {
+        return server.getSavePath(WorldSavePath.ROOT).resolve("reputation.json");
     }
 
     public static int repView(CommandContext<ServerCommandSource> ctx, ServerPlayerEntity entity) {
---
+++ src/main/java/io/github/modrinthsmp/fabricrepsystem/ReputationData.java
@@ -0,0 +1,13 @@
+package io.github.modrinthsmp.fabricrepsystem;
+
+public final class ReputationData {
+    private int reputation;
+
+    public int getReputation() {
+        return reputation;
+    }
+
+    public void setReputation(int reputation) {
+        this.reputation = reputation;
+    }
+}
